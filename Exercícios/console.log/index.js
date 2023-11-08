581a707635e7185a7c6ab5c5019ab037ea13a109
Geovane Mayan <Geovane.mayanlb@gmail.com>
2023-11-08 16:25:05 -0300

    atualizando exercício

---
+++ Exercícios/console.log/index.js
@@ -0,0 +1,8 @@
+console.log("Oi"); // String = Texto
+
+console.log(12345); // Number = Número
+console.log(15.92392);
+
+console.log(10,11,12,13, "Oi");
+
+console.log('Meu nome é "Geovane". Estou aprendendo JavaScript às', 3,'da tarde.');
