90120639ad04f0411e08f0a3ddd36052e12bca8b
Geovane Mayan <Geovane.mayanlb@gmail.com>
2023-11-14 18:08:06 -0300

    novo exercício

---
+++ Exercícios/let/index.js
@@ -0,0 +1,12 @@
+let nome = 'João';
+
+console.log('O nome dele é', nome);
+console.log(nome, 'gosta de futebol');
+
+//Podemos só declarar a variável no começo
+
+let nome2;
+nome2 = 'Pedro';
+console.log('Primeiro nome:', nome2);
+nome2 = 'Gabriel';
+console.log('Segundo nome:', nome2);
