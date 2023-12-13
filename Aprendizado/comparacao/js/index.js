5ba220270cb8d9917262f42665d384b2c82c83bb
Geovane Mayan <Geovane.mayanlb@gmail.com>
2023-12-13 02:04:29 -0300

    Create index.js

---
+++ Aprendizado/comparacao/js/index.js
@@ -0,0 +1,11 @@
+console.log(`1 ${(10 >= 5)}`);  // maior que
+
+console.log(`2 ${(10 >= 5)} ${(5 >= 5)}`); // maior ou igual que
+
+console.log(`3 ${(5 < 10)}`);  // menor que
+
+console.log(`4 ${(5 < 10)} ${(5 <= 5)}`);  // maior ou igual que
+
+console.log(`5 ${(5 === 5)}`);  // igual que
+
+console.log(`6 ${(10 !== 5)}`);  // diferente que
