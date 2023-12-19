64681657422db9f1d30141b7f0e72aa0f567ee25
Geovane Mayan <Geovane.mayanlb@gmail.com>
2023-12-19 14:47:29 -0300

    novo exercício

---
+++ Aprendizado/operacao ternaria/index.js
@@ -0,0 +1,12 @@
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+const pontuacaoUsuario = 999;
+const nivel = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário comum';
+console.log(nivel);
+
+// Igual a isso:
+
+//if (pontuacaoUsuario >=1000){
+//    console.log('Usuário VIP');
+//} else {
+//    console.log ('Usuário comum');
+//}
