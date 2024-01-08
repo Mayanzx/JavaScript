d408de6af13fbe2d9a751bf20975843e6976ef32
Geovane Mayan <Geovane.mayanlb@gmail.com>
2024-01-08 02:13:17 -0300

    Create index.js

---
+++ Aprendizado/break e continue/index.js
@@ -0,0 +1,15 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let numero of numeros){
+
+    if (numero === 2 || numero === 5){
+        continue;
+    }
+
+    if (numero === 7){
+        console.log('Número 7 localizado!')
+        break;
+    }
+
+    console.log(numero);
+}
